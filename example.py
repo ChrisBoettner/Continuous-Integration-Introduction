f6ed38b1dbba4a35d476a1aed283c438fff96b2f
Chris Boettner <boettnec@gmail.com>
2025-02-06 12:42:31 +0100

    put testing into seperate

--- example.py
+++ example.py
@@ -5,7 +5,3 @@ def add(a, b):
 def subtract(a, b):
     return a + b  # do not change this line until prompted to do so.
 
-
-def test_add():
-    assert add(1, 2) == 3
-    assert add(2, 2) == 4
---
+++ test_example.py
@@ -0,0 +1,5 @@
+import example import add, substract
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(2, 2) == 4
